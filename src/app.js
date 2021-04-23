afb1f3b33f44e0b90f58404bfdf61265c1509ece
Johan Garzon <johanprograma@hotmail.com>
2021-04-23 13:06:03 +0200

    Create bibleAPI with most of functions / Add axios module

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const { 
+  getBibleBooks,
+  getBookChapters,
+  getChapterVerses,
+  getSingleVerse,
+  getPassage
+} = require('./resources/bibleAPI/bibleAPI')
 
 const app = express()
 const route = express.Router()
 
 
 // route
-route.get('/bye', function(req, res) {
-  res.send('Chaoooo')
+route.get('/bye', async function(req, res) {
+  const bibleBooks = await getBibleBooks('RVR09')
+  const bookChapters = await getBookChapters('RVR09', 'LEV')
+  const chapterVerses = await getChapterVerses('RVR09', 'DEU.1')
+  const singleVerse = await getSingleVerse('RVR09', 'ACT.1.1')
+  const passage = await getPassage('RVR09', 'MAT.1.12-MAT.1.20')
+
+  res.json(passage)
 })
 
 app.use('/aqui', route)
