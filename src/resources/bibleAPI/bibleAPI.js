fc97c58748316c7f35a2119bf4b3f600c49796a2
Johan Garzon <johanprograma@hotmail.com>
2021-04-26 19:29:16 +0200

    Add getBibles() and separate constans from file

--- src/resources/bibleAPI/bibleAPI.js
+++ src/resources/bibleAPI/bibleAPI.js
@@ -1,80 +1,77 @@
-const axios = require("axios").default
+const axios = require('axios').default
 
-const BASE_URL = 'https://api.scripture.api.bible'
+const {BASE_URL, BIBLES} = require('./constans')
 
-// Bible versions
-const BIBLES = [
-  {
-    id: 'de4e12af7f28f599-02',
-    abbreviation: 'KJV',
-    name: 'King James Version'
-  },
-  {
-    id: '592420522e16049f-01',
-    abbreviation: 'RVR09',
-    name: 'Reina Valera 1909'
-  }
-]
+const bibleAPI = () => {
 
-const _getHeaders = () => {
-  return { 
-    headers: {
-      'api-key': process.env.BIBLE_API_KEY
+  const _getHeaders = () => {
+    return { 
+      headers: {
+        'api-key': process.env.BIBLE_API_KEY
+      }
     }
   }
+
+  const _makeAPIRequest = async url => {
+    return axios.get(url, _getHeaders())
+    .then( response => response.data.data)
+    .catch( error => console.error(error))  
+  }
+
+  const _getBibleID = bibleAbbr => {
+    const bible = BIBLES.find( ({ abbreviation }) => abbreviation === bibleAbbr)
+    return bible.id
+  }
+
+  const getBibles = () => {
+    return BIBLES
+  }
+
+  const getBibleBooks = async bibleAbbr => {
+    const bibleId = _getBibleID(bibleAbbr)
+    const booksUrl = `${BASE_URL}/v1/bibles/${bibleId}/books`
+
+    return _makeAPIRequest(booksUrl)
+  }
+
+  const getBookChapters = async (bibleAbbr, bookId) => {
+    const bibleId = _getBibleID(bibleAbbr)
+    const chaptersUrl = `${BASE_URL}/v1/bibles/${bibleId}/books/${bookId}/chapters`
+    
+    return _makeAPIRequest(chaptersUrl)
+  }
+
+  const getChapterVerses = async (bibleAbbr, chapterId) => {
+    const bibleId = _getBibleID(bibleAbbr)
+    const chapterVersesURL = `${BASE_URL}/v1/bibles/${bibleId}/chapters/${chapterId}/verses`
+
+    return _makeAPIRequest(chapterVersesURL)
+  }
+
+  const getSingleVerse = async (bibleAbbr, verseId) => {
+    const bibleId = _getBibleID (bibleAbbr)
+    const singleVerseUrl = `${BASE_URL}/v1/bibles/${bibleId}/verses/${verseId}`
+    
+    return _makeAPIRequest(singleVerseUrl)
+  }
+
+  // Passage: range of verses when looking for a grouping (i.e. MAT.1.12-MAT.1.20)
+  const getPassage = async (bibleAbbr, versesRange) => {
+    const bibleId = _getBibleID (bibleAbbr)
+    const passageUrl = `${BASE_URL}/v1/bibles/${bibleId}/passages/${versesRange}`
+    
+    return _makeAPIRequest(passageUrl)
+  }
+
+  return {
+    getBibles,
+    getBibleBooks,
+    getBookChapters,
+    getChapterVerses,
+    getSingleVerse,
+    getPassage
+  }
+
 }
 
-const _makeAPIRequest = async url => {
-  return axios.get(url, _getHeaders())
-  .then( response => response.data)
-  .catch( error => console.error(error))  
-}
-
-const _getBibleID = bibleAbbr => {
-  const bible = BIBLES.find( ({ abbreviation }) => abbreviation === bibleAbbr)
-  return bible.id
-}
-
-const getBibleBooks = async bibleAbbr => {
-  const bibleId = _getBibleID(bibleAbbr)
-  const booksUrl = `${BASE_URL}/v1/bibles/${bibleId}/books`
-
-  return _makeAPIRequest(booksUrl)
-}
-
-const getBookChapters = async (bibleAbbr, bookId) => {
-  const bibleId = _getBibleID(bibleAbbr)
-  const chaptersUrl = `${BASE_URL}/v1/bibles/${bibleId}/books/${bookId}/chapters`
-  
-  return _makeAPIRequest(chaptersUrl)
-}
-
-const getChapterVerses = async (bibleAbbr, chapterId) => {
-  const bibleId = _getBibleID(bibleAbbr)
-  const chapterVersesURL = `${BASE_URL}/v1/bibles/${bibleId}/chapters/${chapterId}/verses`
-
-  return _makeAPIRequest(chapterVersesURL)
-}
-
-const getSingleVerse = async (bibleAbbr, verseId) => {
-  const bibleId = _getBibleID (bibleAbbr)
-  const singleVerseUrl = `${BASE_URL}/v1/bibles/${bibleId}/verses/${verseId}`
-  
-  return _makeAPIRequest(singleVerseUrl)
-}
-
-// Passage: range of verses when looking for a grouping (i.e. MAT.1.12-MAT.1.20)
-const getPassage = async (bibleAbbr, versesRange) => {
-  const bibleId = _getBibleID (bibleAbbr)
-  const passageUrl = `${BASE_URL}/v1/bibles/${bibleId}/passages/${versesRange}`
-  
-  return _makeAPIRequest(passageUrl)
-}
-
-module.exports = {
-  getBibleBooks,
-  getBookChapters,
-  getChapterVerses,
-  getSingleVerse,
-  getPassage
-}
+module.exports = bibleAPI()
---
+++ src/resources/bibleAPI/constans.js
@@ -0,0 +1,27 @@
+const BASE_URL = 'https://api.scripture.api.bible'
+
+const BIBLES = [
+    {
+      id: 'de4e12af7f28f599-02',
+      abbreviation: 'KJV',
+      name: 'King James Version',
+      language: 'English'
+    },
+    {
+      id: '65eec8e0b60e656b-01',
+      abbreviation: 'FBV',
+      name: 'Free Bible Version',
+      language: 'English'
+    },
+    {
+      id: '592420522e16049f-01',
+      abbreviation: 'RVR09',
+      name: 'Reina Valera 1909',
+      language: 'Español'
+    }
+  ]
+
+  module.exports = {
+    BASE_URL,
+    BIBLES
+  }
