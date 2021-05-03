1cd8c8d7f58693a2890690e3dfbe4699aa4b2ab6
Johan Garzon <johanprograma@hotmail.com>
2021-05-03 18:58:53 +0200

    Move index.js to root and renamed it as server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const app = require('./app')
+const app = require('./src/app')
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}, from index`)
