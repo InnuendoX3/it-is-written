3f0b33eb86c41803890d00160d44563ed61d5570
Johan Garzon <johanprograma@hotmail.com>
2021-05-04 19:19:34 +0200

    Serve static React assets if in production

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 require('dotenv').config()
+const express = require('express')
+const path = require('path')
+
 const app = require('./src/app')
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  
+  // Set static files from the React app
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}, from index`)
