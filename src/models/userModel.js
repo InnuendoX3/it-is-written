a24973f9a12fe093f71d238f8d1f8c12a2046923
Johan Garzon <johanprograma@hotmail.com>
2021-05-14 17:26:01 +0200

    Hash password on sign on // check if user exists before sign in

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const { errorResponse } = require('../controllers/responseHandler')
 
 const userSquema = new mongoose.Schema({
   username: { type: String, required: true },
@@ -8,12 +10,16 @@ const userSquema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSquema)
 
-const createUser = (userData) => {
+const createUser = async userData => {
   const userToSave = {
     username: userData.username,
     email: userData.email,
-    password: userData.password
+    password: bcrypt.hashSync(userData.password, 10)
   }
+
+  const userExists = await User.findOne({ email: userToSave.email })
+
+  if(userExists) throw errorResponse(400, 'User already exists')
 
   return User.create(userToSave)
     .then( data => data)
