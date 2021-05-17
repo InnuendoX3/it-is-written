78542b8e8956dc0fd266a3d4b9931eac203e8632
Johan Garzon <johanprograma@hotmail.com>
2021-05-17 10:14:00 +0200

    Debug heroku: Add routes again on app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 
 const app = express()
-//const routes = require('./routes')
+const routes = require('./routes')
 //const dbConnect = require('./db')
 
 //dbConnect()
 
 app.use(express.json())
-//app.use('/api', routes)
+app.use('/api', routes)
 app.use('/saludo', (req, res) => {
   res.status(200).send({saludo: 'Hello!2'})
 })
